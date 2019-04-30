526fa4a29593b662317973eeeeed71e53fd47f02
Nicolas <marty.nicolas11@laposte.net>
2019-04-30 15:52:46 +0200

    Correction bug

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +11,13 @@ constructor() { }
 
 rechercherParNom(nom: string): string[]  {
   // TODO retourner une liste de matricules fictifs à partir du fichier `src/app/mock/matricules.mock.ts`.  
+  return [""];
 }
 
 recupererCollegueCourant(): Collegue {
   // TODO retourner le collègue fictif à partir du fichier `src/app/mock/collegues.mock.ts`.
+  let temp = new Collegue("a1", "babar", "bebe", new Date(1988,2,1), "http://dsf", "az@analyzeAndValidateNgModules.com");
+  return temp;
 }
 
 }
