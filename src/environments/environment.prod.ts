d0dff806a76f9859bd270cf799913c525114ae4a
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-05-10 20:00:00 +0200

    Tentative de résolution du bug sur Travis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: `https://nicolas-collegues-api.herokuapp.com`
+  backendUrl: `https://nicolas-collegues-api.herokuapp.com/collegues`
 };
